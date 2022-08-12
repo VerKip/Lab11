fbc85a11bfa7e677f75f4bc06adafc8ce5d68c05
Vera Kiprskaya <vera.malyutina@gmail.com>
2022-08-12 19:03:06 +0300

    Add project files.

---
+++ Lab11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения k и b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Result result = new Result(k,b);
+            Console.WriteLine(result.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab11/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    struct Result
+    {
+        double k;
+        double b;
+
+        public Result(double k, double b)
+        {
+            this.k = k; 
+            this.b = b;
+        }
+
+        public string Root()
+        {
+            if (k == 0 && b != 0)
+                return "Решений нет";
+            if (k == 0 && b == 0)
+                return "х - любое значение";
+            double r = -b / k;
+                return $"Решение = {r}";
+        }
+
+    }
+}
